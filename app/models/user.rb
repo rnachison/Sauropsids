cb56c0db9454be96f570473084bdafe087f472b4
rnachison <rnachison@gmail.com>
2014-11-06 11:22:48 -0800

    make user page link in header/style ptero_index page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,12 @@ class User
   include ActiveModel::SecurePassword
   field :email, type: String
   field :password_digest, type: String
+  field :name, type: String
 
   has_secure_password
 
   validates_uniqueness_of :email
+  validates_presence_of :name
 
   has_many :sauropsids
 end
